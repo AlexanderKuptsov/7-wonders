ff593bf6a2c47d1ab1a96401bb0c42b55f33c073
Alexander Kuptsov <shon55398@gmail.com>
2020-11-14 22:15:53 +0300

    base card visualization system

---
+++ 7_Wonders/Assets/_ProjectContent/Scripts/GameLogic/Cards/Visualization/CardVisualizer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace WhiteTeam.GameLogic.Cards.Visualization
+{
+    public abstract class CardVisualizer<T> : MonoBehaviour
+        where T : CardData
+    {
+        public T CardData;
+    }
+}
