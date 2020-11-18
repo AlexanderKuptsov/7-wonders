d834a21321f7e6914f54b5f0d393fc185ec7822e
Alexander Kuptsov <shon55398@gmail.com>
2020-11-18 15:22:43 +0300

    cards stack

---
+++ 7_Wonders/Assets/_ProjectContent/Scripts/GameLogic/Cards/CardsStack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace WhiteTeam.GameLogic.Cards
+{
+    public static class CardsStack
+    {
+        private static readonly Dictionary<GameState.EpochType, Queue<Card>>
+            Cards = new Dictionary<GameState.EpochType, Queue<Card>>
+            {
+                {GameState.EpochType.FIRST, new Queue<Card>()},
+                {GameState.EpochType.SECOND, new Queue<Card>()},
+                {GameState.EpochType.THIRD, new Queue<Card>()}
+            };
+
+        private static readonly Dictionary<GameState.EpochType, List<Card>> ThrownCards =
+            new Dictionary<GameState.EpochType, List<Card>>
+            {
+                {GameState.EpochType.FIRST, new List<Card>()},
+                {GameState.EpochType.SECOND, new List<Card>()},
+                {GameState.EpochType.THIRD, new List<Card>()}
+            };
+
+        public static void LoadCards(GameState.EpochType epoch, IEnumerable<Card> cards)
+        {
+            foreach (var card in cards)
+            {
+                Cards[epoch].Enqueue(card);
+            }
+        }
+
+        public static Card TakeCard(GameState.EpochType epoch)
+        {
+            return Cards[epoch].Dequeue();
+        }
+
+        public static void ThrowCard(Card card, GameState.EpochType epoch)
+        {
+            ThrownCards[epoch].Add(card);
+        }
+
+        public static IEnumerable<Card> GetThrownCards(GameState.EpochType epoch)
+        {
+            return ThrownCards[epoch];
+        }
+    }
+}
