0e74cefe53b6e7403f071d38d8f34274eccc20af
Alexander Kuptsov <shon55398@gmail.com>
2020-11-20 18:55:42 +0300

    base selectable effect interface

---
+++ 7_Wonders/Assets/_ProjectContent/Scripts/GameLogic/Cards/Effects/ISelectableResourceEffect.cs
@@ -0,0 +1,9 @@
+﻿using WhiteTeam.GameLogic.Resources;
+
+namespace WhiteTeam.GameLogic.Cards.Effects
+{
+    public interface ISelectableResourceEffect<T>
+    {
+        Resource.CurrencyBaseData<T> Select();
+    }
+}
