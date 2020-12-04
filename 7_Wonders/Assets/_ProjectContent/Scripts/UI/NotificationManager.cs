fcd457675096627fb6c12a5dbf84696932c2390b
Alexander Kuptsov <shon55398@gmail.com>
2020-12-04 17:29:17 +0300

    in-game notifications

---
+++ 7_Wonders/Assets/_ProjectContent/Scripts/UI/NotificationManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace WhiteTeam.UI
+{
+    public class NotificationManager : Singleton<NotificationManager>
+    {
+        [SerializeField] private PopUpWindow warningWindow;
+
+        public void Warning(string text)
+        {
+            warningWindow.Object.SetActive(true);
+            warningWindow.Text.text = text;
+        }
+
+        [Serializable]
+        public struct PopUpWindow
+        {
+            public GameObject Object;
+            public TMP_Text Text;
+        }
+    }
+}
