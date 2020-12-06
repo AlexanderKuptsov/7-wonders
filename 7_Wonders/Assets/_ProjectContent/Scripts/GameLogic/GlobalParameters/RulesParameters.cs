79ae28afb0a642e6ade83d66bd82305bf29801ed
Alexander Kuptsov <shon55398@gmail.com>
2020-12-06 20:59:09 +0300

    score handling

--- 7_Wonders/Assets/_ProjectContent/Scripts/GameLogic/GlobalParameters/RulesParameters.cs
+++ 7_Wonders/Assets/_ProjectContent/Scripts/GameLogic/GlobalParameters/RulesParameters.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using MyBox;
+using UnityEngine;
 
 namespace WhiteTeam.GameLogic.GlobalParameters
 {
@@ -9,13 +10,16 @@ namespace WhiteTeam.GameLogic.GlobalParameters
         [PositiveValueOnly] public int CardExchangeAmount = 3;
         [PositiveValueOnly] public int ResourceDefaultBuyCost = 2;
 
+        [Header("Score")] public int MoneyToVictoryPoint = 3;
+        public int DifferentScienceGroupModifier = 7;
+
         public Dictionary<GameState.EpochType, int> EpochWarVictoryTokens = new Dictionary<GameState.EpochType, int>
         {
             {GameState.EpochType.FIRST, 1},
             {GameState.EpochType.SECOND, 3},
             {GameState.EpochType.THIRD, 5},
         };
-        
+
         public Dictionary<GameState.EpochType, int> EpochWarLoseTokens = new Dictionary<GameState.EpochType, int>
         {
             {GameState.EpochType.FIRST, 1},
