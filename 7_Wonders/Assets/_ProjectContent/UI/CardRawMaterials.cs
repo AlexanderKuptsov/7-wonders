88afb82ebc74d2a02d4096d4691914020eb0653c
Luc Prucell <lucprucell@gmail.com>
2020-12-09 21:03:19 +0300

    Connecting with UI

---
+++ 7_Wonders/Assets/_ProjectContent/UI/CardRawMaterials.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName="New Card",menuName="Cards/RawMaterials/1")]
+public class CardRawMaterials : CardData
+
+{
+   public override Color change_color()
+   {
+      return  new Color32(139,69,19,255);
+   }
+
+}
