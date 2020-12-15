a85bf4e8306966fe5230b4f8c10650a891a0d68f
Alexander Kuptsov <shon55398@gmail.com>
2020-12-15 14:09:02 +0300

    CardVisualizationController example

---
+++ 7_Wonders/Assets/_ProjectContent/Scripts/GameLogic/Cards/Visualization/CardObjectVisualSetter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace WhiteTeam.GameLogic.Cards.Visualization
+{
+    public class CardObjectVisualSetter : MonoBehaviour
+    {
+        public void SetName(string cardName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetColor(Color cardColor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetEffect(string cardName)
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO ...
+    }
+}
