9bfd6d6703b6cfb3c9583f287766c20d4392234c
Aleksei Zalata <alekszalata@gmail.com>
2021-01-15 02:30:42 +0300

    LeaveToLobbyButton

---
+++ 7_Wonders/Assets/_ProjectContent/Scripts/ConnectUI/QuitToLobby.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using WhiteTeam.ConnectingUI;
+
+public class QuitToLobby : MonoBehaviour
+{
+
+    public void ChangeSceneToLobby()
+    {
+        SceneChanger.Instance.OpenLobbyScene();
+    }
+}
