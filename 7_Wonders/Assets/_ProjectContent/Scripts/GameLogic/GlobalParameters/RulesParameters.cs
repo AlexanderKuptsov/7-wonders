2830e4dba69e4f6002eac9c95a5331eddb1ba133
Alexander Kuptsov <shon55398@gmail.com>
2021-01-15 03:01:54 +0300

    more cards objects

--- 7_Wonders/Assets/_ProjectContent/Scripts/GameLogic/GlobalParameters/RulesParameters.cs
+++ 7_Wonders/Assets/_ProjectContent/Scripts/GameLogic/GlobalParameters/RulesParameters.cs
@@ -12,19 +12,5 @@ namespace WhiteTeam.GameLogic.GlobalParameters
 
         [Header("Score")] public int MoneyToVictoryPoint = 3;
         public int DifferentScienceGroupModifier = 7;
-
-        public Dictionary<GameState.EpochType, int> EpochWarVictoryTokens = new Dictionary<GameState.EpochType, int>
-        {
-            {GameState.EpochType.FIRST, 1},
-            {GameState.EpochType.SECOND, 3},
-            {GameState.EpochType.THIRD, 5},
-        };
-
-        public Dictionary<GameState.EpochType, int> EpochWarLoseTokens = new Dictionary<GameState.EpochType, int>
-        {
-            {GameState.EpochType.FIRST, 1},
-            {GameState.EpochType.SECOND, 1},
-            {GameState.EpochType.THIRD, 1},
-        };
     }
 }
