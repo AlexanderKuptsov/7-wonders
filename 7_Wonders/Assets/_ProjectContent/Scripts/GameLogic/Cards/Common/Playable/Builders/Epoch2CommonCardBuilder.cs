7680374e328403a094ba21483cdfd3584e8f6235
Alexander Kuptsov <shon55398@gmail.com>
2021-02-13 20:59:30 +0300

    epoch 2 & epoch 3 card builders

---
+++ 7_Wonders/Assets/_ProjectContent/Scripts/GameLogic/Cards/Common/Playable/Builders/Epoch2CommonCardBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace WhiteTeam.GameLogic.Cards
+{
+    public class Epoch2CommonCardBuilder : ICommonCardBuilder
+    {
+        public IEnumerable<RawMaterialsCard> CreateRawMaterialsCard()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ProductionCard> CreateProductionCard()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<CivilianCard> CreateCivilianCard()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<MilitaryCard> CreateMilitaryCard()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<CommercialMoneyCard> CreateTradeCard()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ScientificCard> CreateScientificCard()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
